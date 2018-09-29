c629636c19d616a355b539be28747a39336e861d
Keven Lefebvre <orditeck@gmail.com>
2018-09-29 13:07:06 -0400

    ignore deploy route

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Http\Middleware;
 
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
@@ -18,7 +17,5 @@ class VerifyCsrfToken extends Middleware
      *
      * @var array
      */
-    protected $except = [
-        //
-    ];
+    protected $except = ['deploy/github'];
 }
